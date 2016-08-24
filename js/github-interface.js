673486323bfd16995bc1038c028bc4313d2475af
russvetsper <russvetsper@gmail.com>
2016-08-23 23:32:48 -0700

    add for loop to js and add a ul to index

--- js/github-interface.js
+++ js/github-interface.js
@@ -1,13 +1,10 @@
 var Repo = require("./../js/github.js").repoModule;
 
-
-
-
 $(document).ready(function() {
   $('#getName').click(function() {
     var name = $('#name').val();
     $('#name').val("");
-    alert(name);
+
     var repoObject = new Repo();
     repoObject.getRepos(name);
   });
--- js/github.js
+++ js/github.js
@@ -1,18 +1,25 @@
 var apiKey = require('./../.env').apiKey;
 
+
 function Repo() {
 }
 
+
 Repo.prototype.getRepos = function(name) {
-  $.get('https://api.github.com/users/' + input +'?access_token=' + apiKey).then(function(response){
-    console.log(JSON.stringify(response.repos_url));
+  $.get('https://api.github.com/users/' + name +'/repos?/access_token=' + apiKey).then(function(response){
+    console.log(JSON.stringify(response));
+
+
+
+
+    for(var i = 0; i <= response.length; i++){
+      $('ul#repos').append("<li>" + response[i].name + "</li>");
+    }
+
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
 };
-
-
-
 
 
 exports.repoModule = Repo;
